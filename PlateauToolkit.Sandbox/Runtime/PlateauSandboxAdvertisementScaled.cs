f1fed9a91f01618667257db51a0717dfa299d775
kaichi.oda@synesthesias.jp <kaichi.oda@synesthesias.jp>
2025-08-05 10:36:54 +0900

    サイズ変更可能なAdvertiseAsset向けスクリプト追加

---
+++ PlateauToolkit.Sandbox/Runtime/PlateauSandboxAdvertisementScaled.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace PlateauToolkit.Sandbox.Runtime
+{
+    public class PlateauSandboxAdvertisementScaled : PlateauSandboxPlaceableHandler
+    {
+        [SerializeField]
+        private GameObject billboardRoot;
+
+        [SerializeField]
+        private GameObject poleRoot;
+
+        private Vector3 billboardDefaultPosition;
+        private Vector3 poleDefaultPosition;
+
+        public Vector3 BillboardSize
+        {
+            get => billboardRoot.transform.localScale;
+            set
+            {
+                billboardRoot.transform.localScale = value;
+            }
+        }
+
+        public float PoleHeight
+        {
+            get => poleRoot.transform.localScale.y;
+            set
+            {
+                poleRoot.transform.localScale = new Vector3(poleRoot.transform.localScale.x, value, poleRoot.transform.localScale.z);
+
+                // 高さの値に応じてbillboardRootのy座標を変更する
+                billboardRoot.transform.localPosition = new Vector3(billboardRoot.transform.localPosition.x, poleRoot.transform.localScale.y, billboardRoot.transform.localPosition.z);
+            }
+        }
+
+        private void Awake()
+        {
+            billboardDefaultPosition = billboardRoot.transform.position;
+            poleDefaultPosition = poleRoot.transform.position;
+        }
+    }
+}
