6ec042daba21282310d37fad2c55ee00131c9b53
gzoe-ymaenami <ymaenami@gzoe.co.jp>
2025-10-14 21:03:51 +0900

    Landscape-Design-Tool-3のアセット配置機能の改修用の機能追加 (#68)
    * fix(PlateauSandboxAdvertisementScaled): PoleHeightの設定方法を修正
    
    * wip(PlateauSandboxAdvertisementScaled): 動画プレイヤーとマテリアル設定機能を追加中
    
    * feat(PlateauSandboxBuilding): サイズ変更パネルの表示機能を追加
    
    サイズ変更パネルの表示/非表示を制御するプロパティを追加しました。
    
    ---------
    
    Co-authored-by: syn-yusuke-maenami <195725624+syn-yusuke-maenami@users.noreply.github.com>

--- PlateauToolkit.Sandbox/Runtime/PlateauSandboxAdvertisementScaled.cs
+++ PlateauToolkit.Sandbox/Runtime/PlateauSandboxAdvertisementScaled.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace PlateauToolkit.Sandbox.Runtime
 {
@@ -16,6 +19,16 @@ namespace PlateauToolkit.Sandbox.Runtime
         //private Vector3 billboardDefaultPosition;
         //private Vector3 poleDefaultPosition;
 
+
+        // 画像と動画表示用
+        public int targetMaterialNumber;
+        public string targetTextureProperty = "_MainTex";
+        public PlateauSandboxAdvertisement.AdvertisementType advertisementType;
+        public List<PlateauSandboxAdvertisement.AdvertisementMaterials> advertisementMaterials;
+        public Texture advertisementTexture;
+        public VideoClip advertisementVideoClip;
+        public VideoPlayer VideoPlayer { get; private set; }
+
         /// <summary>
         /// ワールド空間でのサイズ
         /// </summary>
@@ -27,7 +40,7 @@ namespace PlateauToolkit.Sandbox.Runtime
             }
             set
             {
-                Vector3 v =  new Vector3(value.x / transform.lossyScale.x, value.y / transform.lossyScale.y, value.z / transform.lossyScale.z);
+                Vector3 v = new Vector3(value.x / transform.lossyScale.x, value.y / transform.lossyScale.y, value.z / transform.lossyScale.z);
                 billboardRoot.transform.localScale = v;
             }
         }
@@ -43,7 +56,7 @@ namespace PlateauToolkit.Sandbox.Runtime
             }
             set
             {
-                float y = value / transform.parent.lossyScale.y;
+                float y = value / transform.lossyScale.y;
                 poleRoot.transform.localScale = new Vector3(poleRoot.transform.localScale.x, y, poleRoot.transform.localScale.z);
 
                 // 高さの値に応じてbillboardRootのy座標を変更する
@@ -67,11 +80,79 @@ namespace PlateauToolkit.Sandbox.Runtime
             billboardRoot.transform.hasChanged = false;
         }
 
-        private void Awake()
+        public void AddVideoPlayer()
         {
-            //billboardDefaultPosition = billboardRoot.transform.position;
-            //poleDefaultPosition = poleRoot.transform.position;
+            if (!gameObject.TryGetComponent(out VideoPlayer videoPlayer))
+            {
+                videoPlayer = gameObject.AddComponent<VideoPlayer>();
+                videoPlayer.isLooping = true;
+                videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+                videoPlayer.clip = advertisementVideoClip;
+                VideoPlayer = videoPlayer;
+            }
+            else
+            {
+                VideoPlayer = videoPlayer;
+            }
         }
+
+        public void SetTexture()
+        {
+            if (advertisementMaterials.Count <= 0)
+            {
+                return;
+            }
+
+            Material mat = advertisementMaterials[0].materials[targetMaterialNumber];
+            if (mat.HasProperty(targetTextureProperty))
+            {
+                // RenderTextureは設定しない
+                Texture texture = mat.GetTexture(targetTextureProperty);
+                if (texture as RenderTexture == null)
+                {
+                    advertisementTexture = texture;
+                }
+            }
+        }
+
+        public void SetMaterials()
+        {
+            MeshRenderer[] lsChildMeshRender = transform.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer childMeshRender in lsChildMeshRender)
+            {
+                IEnumerable<PlateauSandboxAdvertisement.AdvertisementMaterials> matchingMaterials =
+                    advertisementMaterials.Where(m => m.gameObjectName == childMeshRender.transform.name);
+
+                foreach (PlateauSandboxAdvertisement.AdvertisementMaterials advertisementMaterial in matchingMaterials)
+                {
+                    childMeshRender.sharedMaterials = advertisementMaterial.materials.ToArray();
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            advertisementMaterials = new List<PlateauSandboxAdvertisement.AdvertisementMaterials>();
+            MeshRenderer[] lsChildMeshRender = transform.GetComponentsInChildren<MeshRenderer>();
+            foreach (MeshRenderer childMeshRender in lsChildMeshRender)
+            {
+                var advertisementMaterial = new PlateauSandboxAdvertisement.AdvertisementMaterials
+                {
+                    gameObjectName = childMeshRender.transform.name,
+                    materials = new List<Material>(childMeshRender.sharedMaterials)
+                };
+                advertisementMaterials.Add(advertisementMaterial);
+            }
+
+            SetTexture();
+            AddVideoPlayer();
+        }
+
+        // private void Awake()
+        // {
+        //     //billboardDefaultPosition = billboardRoot.transform.position;
+        //     //poleDefaultPosition = poleRoot.transform.position;
+        // }
 
     }
 }
